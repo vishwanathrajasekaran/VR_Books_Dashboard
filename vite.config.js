0fb51d524b2f0a05d02840470abcf030afcd4738
Vishwanath Rajasekaran <127646664+vishwanathrajasekaran@users.noreply.github.com>
2026-05-10 12:59:37 +0530

    Add build and server configurations to Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+  },
 })
